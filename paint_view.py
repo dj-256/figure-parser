268dd722b62c03a36a265a30815183093629dfaf
Joel <joel@dibasso.fr>
2023-02-15 11:12:36 +0100

    White background

--- paint_view.py
+++ paint_view.py
@@ -3,7 +3,6 @@ from PySide6.QtCore import Qt, QRectF
 from PySide6.QtGui import QPen, QPainter, QPainterPath, QBrush, QImage
 from PySide6.QtWidgets import QGraphicsView, QGraphicsScene
 from skimage.transform import resize
-from skimage.util import invert
 
 
 class PaintView(QGraphicsView):
@@ -17,6 +16,7 @@ class PaintView(QGraphicsView):
         self.path = None
         self.current_item = None
         self.setSceneRect(QRectF(0, 0, 280, 280))
+        self.setBackgroundBrush(QBrush(Qt.white))
 
     def mousePressEvent(self, event):
         pos = self.mapToScene(event.pos())
@@ -46,11 +46,10 @@ class PaintView(QGraphicsView):
         data = image.bits()
         arr = np.frombuffer(data, np.uint8).reshape((image.height(), image.width(), 4))
         arr = resize(arr, (28, 28), preserve_range=True)
-        # Convert the ARGB format to grayscale and normalize to [0, 1]
+        # Convert the ARGB format to grayscale
         arr = arr[..., 0]  # Select only the alpha channel (grayscale)
         # arr = invert(arr)
         arr = 255 - arr.astype(np.uint8)
-        print(arr)
         self.scene().clear()
         self.update()
         return arr
